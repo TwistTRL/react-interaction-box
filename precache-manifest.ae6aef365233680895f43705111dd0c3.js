ddee3004a018db27cff5bb56930b2450d9a14847
Lingyu Zhou <zhoulytwin@gmail.com>
2019-05-31 12:07:31 -0400

    Updates

---
+++ precache-manifest.ae6aef365233680895f43705111dd0c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c721f5f106d83e7ad1f30bc1a91bb00e",
+    "url": "/react-plot-interaction-box/index.html"
+  },
+  {
+    "revision": "ffff190c151fae71ffcc",
+    "url": "/react-plot-interaction-box/static/css/main.1578f53f.chunk.css"
+  },
+  {
+    "revision": "6056431db9595d90ab14",
+    "url": "/react-plot-interaction-box/static/js/2.d65f078d.chunk.js"
+  },
+  {
+    "revision": "ffff190c151fae71ffcc",
+    "url": "/react-plot-interaction-box/static/js/main.1e8f7fe7.chunk.js"
+  },
+  {
+    "revision": "4821ccf264242146798d",
+    "url": "/react-plot-interaction-box/static/js/runtime~main.1cd30d31.js"
+  }
+]);
